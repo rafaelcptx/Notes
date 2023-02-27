9bf5d56579a2ef915065bbee072b69d7e27ad154
rafaelcptx <rafaelteixeiradesigner@gmail.com>
2023-02-27 17:17:12 -0300

    implementa metodos crud nos buttons do front-end

--- client/src/components/CardNote.jsx
+++ client/src/components/CardNote.jsx
@@ -1,17 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style-priority.css";
 import "./style.css";
+import api from "../services/api";
 
 const CardNote = ({ data }) => {
+  const [changedNote, setChangedNote] = useState("");
+
+  const handleEdit = async (e, note) => {
+    if (changedNote && changedNote != note) {
+      await api.post(`/content/${data._id}`, { note: changedNote });
+      alert(`Note: "${data.title}" successfully edited!`);
+    }
+  };
+
+  const handleDelete = async () => {
+    let response = prompt(
+      `Are you sure that you want to delete "${data.title}" note? Input "yes" to confirm.`
+    );
+    if (response == "yes") {
+      await api.delete(`/notes/${data._id}`);
+    }
+  };
+
+  const handlePriority = async () => {
+    await api.post(`/priorities/${data._id}`);
+  };
+
   return (
     <>
       <li className={data.priority ? "priorityCardNote" : "cardNote"}>
         <div>
           <strong>{data.title}</strong>
-          <div>Delete</div>
+          <div onClick={handleDelete}>Delete</div>
         </div>
-        <textarea readOnly defaultValue={data.note} />
-        <span>• Important!</span>
+        <textarea
+          onChange={(e) => setChangedNote(e.target.value)}
+          onBlur={(e) => handleEdit(e.target.value, data.note)}
+          defaultValue={data.note}
+        />
+        <span onClick={handlePriority}>• Important!</span>
       </li>
     </>
   );
