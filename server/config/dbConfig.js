27d348980b14715deaac7ca44f7beea112fa1ad4
rafaelcptx <rafaelteixeiradesigner@gmail.com>
2023-02-28 11:12:35 -0300

    add .env

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
+
 mongoose.set("strictQuery", true);
 const connection = mongoose
-  .connect(
-    "mongodb+srv://dbUser:dbPassword@cluster0.co9uqcb.mongodb.net/notes?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(console.log("Conectado ao DB com sucesso!"));
 
 module.exports = connection;
